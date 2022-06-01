ab96f2090402efaf8b3439a58823e1d36d154120
meisReal <simas.cesnakauskas@gmail.com>
2022-06-01 07:22:35 +0300

    bones for operate function that calls math functions

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 let calculator = {
     add: function (a, b) {
+        console.log(a + b);
         return a + b;
+
     },
     subtract: function (a, b) {
+        console.log(a - b);
         return a - b;
     },
     multiply: function (a, b) {
+        console.log(a * b);
         return a * b;
     },
     divide: function (a, b) {
+        console.log(a / b);
         return a / b;
+    }
+}
+
+function operate() {
+    let a = prompt();
+    let b = prompt();
+    switch (prompt()) {
+        case '+':
+            calculator.add(a, b);
+            break;
+        case '-':
+            calculator.subtract(a, b);
+            break;
+        case '*':
+            calculator.multiply(a, b);
+            break;
+        case '/':
+            calculator.divide(a, b);
+            break;
+        default:
+            console.log('WHOOPS');
     }
 }
