9a7a0dc12ff56af36109a8f130e43f96972d572e
meisReal <simas.cesnakauskas@gmail.com>
2022-06-04 09:35:46 +0300

    calculation now work in seqence

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let displayText = [];
 let operand = '';
+let operandB = '';
 let numA;
 let numB;
 const display = document.querySelector('.display');
@@ -9,39 +10,57 @@ let calculator = {
     add: function (a, b) {
         result = a + b;
         display.textContent = result;
-        displayText.splice(0, displayText.length);
-        displayText.splice(1, 0, result);
+        numB = result;
+        displayText = [];
     },
     subtract: function (a, b) {
         result = a - b;
         display.textContent = result;
-        displayText.splice(0, displayText.length);
-        displayText.splice(1, 0, result);
+        numB = result;
+        displayText = [];
     },
     multiply: function (a, b) {
         result = a * b;
         display.textContent = result;
-        displayText.splice(0, displayText.length);
-        displayText.splice(1, 0, result);
+        numB = result;
+        displayText = [];
     },
     divide: function (a, b) {
         result = a / b;
         display.textContent = result;
-        displayText.splice(0, displayText.length);
-        displayText.splice(1, 0, result);
+        numB = result;
+        displayText = [];
     }
 }
 
 
 function operate() {
-    cutArray();
+    let temp;
+    operand = this.textContent;
+    temp = operandB;
+    operandB = operand;
+    operand = temp;
+
+    swapNumbers();
     if (operand == '+') {
+        if (numA == undefined) {
+            return;
+        }
         calculator.add(numA, numB);
     } else if (operand == '-') {
+        if (numA == undefined) {
+            return;
+        }
         calculator.subtract(numA, numB);
     } else if (operand == '/') {
+        if (numA == undefined) {
+            return;
+        }
         calculator.divide(numA, numB);
     } else if (operand == '*') {
+        if (numA == undefined) {
+            return;
+        }
         calculator.multiply(numA, numB);
     } else {
         return;
@@ -50,29 +69,22 @@ function operate() {
 
 function populateDisplay() {
 
-    // const display = document.querySelector('.display');
     displayText.push(this.textContent);
     display.textContent = displayText.join('');
+}
 
 
-    console.log(displayText)
+
+function swapNumbers() {
+    let temp;
+    numA = Number(displayText.splice(0, displayText.length).join(''));
+    temp = numB;
+    numB = numA;
+    numA = temp;
 
 }
 
-function cutArray() {
-    for (i = 0; i < displayText.length; i++) {
-        if (displayText[i] == '+' || displayText[i] == '-' || displayText[i] == '*' || displayText[i] == '/') {
-            operand = displayText[i]
-            numA = Number(displayText.splice(0, i).join(''));
-            numB = Number(displayText.slice(1).join(''));
 
-        }
-    }
-
-    // console.log(numA);
-    // console.log(operand);
-    // console.log(numB);
-}
 
 
 const btns = document.querySelectorAll('.button');
@@ -82,3 +94,8 @@ for (i = 0; i < btns.length; i++) {
 
 const evalButton = document.querySelector('#equal');
 evalButton.addEventListener('click', operate);
+
+const operationButton = document.querySelectorAll('.buttonOperation');
+for (i = 0; i < operationButton.length; i++) {
+    operationButton[i].addEventListener('click', operate)
+}
